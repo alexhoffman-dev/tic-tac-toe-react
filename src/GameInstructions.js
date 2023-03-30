c2e6625c53548182c2b2c025aff5287bbba66dcd
Luke Donahue <luke@expensify.com>
2023-03-30 14:04:25 -0700

    two more typos

--- src/GameInstructions.js
+++ src/GameInstructions.js
@@ -4,7 +4,7 @@ const GameInstructions = ({gameBuild}) => {
     // If the  gameBuild is null then display the first set of instructions.
     // Otherwise display the how to win instructions.
     const preGameInstructions = `Instructions: Select a board size by submitting a number in the field below vvvv`;
-    let instructions = gameBuild === null ? preGameInstructions : `Instructions: Weclome players, get ${gameBuild.boardSize} marks in a row (up/down , side/side, or diagonally), to win!`;;
+    let instructions = gameBuild === null ? preGameInstructions : `Instructions: Welcome players, get ${gameBuild.boardSize} marks in a row (up/down , side/side, or diagonally), to win!`;
 
     return (
         <h3>
